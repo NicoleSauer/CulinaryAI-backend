9b9a0f8a8955e0a6d8bbb6e29d59b924471fe7e3
Nicole Sauer <95630400+NicoleSauer@users.noreply.github.com>
2023-12-06 23:09:57 +0100

    added script to generate training data

--- data/data_transformation/struct_data_to_train_data.py
+++ data/data_transformation/struct_data_to_train_data.py
@@ -1,56 +1,133 @@
 import json
+from itertools import combinations
+from tqdm import tqdm
+import random
 
-DATA = './data/data_acquisition/structured_recipes.json'
+# Load the original JSON data
+with open('./data/data_acquisition/recipes_corrected.json', 'r', encoding='utf-8') as file:
+    recipes = json.load(file)
 
-# Load the JSON file
-with open(DATA, 'r', encoding="utf-8") as file:
-    recipes_data = json.load(file)
 
-# Extract ingredients list and generate prompts for each recipe
-output_data = []
+def generate_combinations(ingredients):
+    all_combinations = []
+    # Generate combinations of all lengths from 1 to the length of ingredients list
+    for r in range(1, len(ingredients) + 1):
+        # Use combinations to generate all possible combinations of length r
+        combinations_r = combinations(ingredients, r)
+        # Extend the all_combinations list with the combinations generated
+        all_combinations.extend(combinations_r)
 
-for recipe in recipes_data:
-    ingredients = [ingredient['name'] for ingredient in recipe['Ingredients']]
-    prompts = {
-        "Id": recipe["Id"],
-        "Name": recipe["Name"],
-        "Ingredients": ingredients,
+    return all_combinations
+
+
+def filter_available_ingredients(ingredients, available_ingredients):
+    # Filter out ingredients that are not in available_ingredients
+    non_available_ingredients = []
+    for ingredient in ingredients:
+        if ingredient not in available_ingredients:
+            non_available_ingredients.append(ingredient)
+    return non_available_ingredients
+
+
+def comma_separate_list(list, key='name'):
+    # Separate list with comma
+    comma_separated_list = ''
+
+    if len(list) == 1:
+        comma_separated_list = list[0][key]
+    elif len(list) == 2:
+        comma_separated_list = f"{list[0][key]} und {list[1][key]}"
+    else:
+        for item in list:
+            if item == list[-2]:
+                comma_separated_list += f"{item[key]}" + " und "
+            elif item == list[-1]:
+                comma_separated_list += f"{item[key]}"
+            else:
+                comma_separated_list += f"{item[key]}" + ", "
+    return comma_separated_list
+
+
+def generate_full_ingredient_string(ingredients):
+    # Generate a string of all ingredients
+    full_ingredient_string = ''
+    for ingredient in ingredients:
+        if ingredient['amount'] is not None:
+            full_ingredient_string += f"{ingredient['amount']} "
+        if ingredient['unit'] is not None:
+            full_ingredient_string += f"{ingredient['unit']} "
+        if ingredient['name'] is not None:
+            full_ingredient_string += f"{ingredient['name']}, "
+    return full_ingredient_string
+
+
+# Function to generate prompts and answers for each recipe
+def generate_training_data(recipe):
+    training_data = []
+    ingredients = recipe['ingredients']
+    prep_time = recipe['prepTime']
+    portions = recipe['numberOfPortions']
+    nutritional_values = recipe['nutritionalValues']
+
+    # generate a list of all different ingredient combinations
+    ingredient_combinations = generate_combinations(ingredients)
+
+    # Limit the number of ingredient combinations per recipe to 100
+    if len(ingredient_combinations) > 100:
+        # print(
+        #    f"Number of ingredient combinations for recipe {recipe['name']} is greater than 100. Randomly selecting 100 ingredient combinations.")
+        # Randomly select 100 ingredient combinations
+        ingredient_combinations = random.sample(
+            ingredient_combinations, 100)
+
+    for ingredient_combination in ingredient_combinations:
+        available_ingredients_string = comma_separate_list(
+            ingredient_combination)
+
+        # Finding missing ingredients not in ingredient_combination
+        non_available_ingredients_string = comma_separate_list(filter_available_ingredients(
+            ingredients, ingredient_combination))
+
+        prompt = available_ingredients_string
+
+        if len(non_available_ingredients_string) == 0:
+            answer = f'Du hast alle Zutaten für das folgende Rezept: {recipe["name"]}. '
+        else:
+            answer = f'Wenn du noch {non_available_ingredients_string} besorgst, kannst du das folgende Rezept nachkochen: {recipe["name"]}. '
+        answer += 'Dazu brauchst diese Zutaten: ' + \
+            generate_full_ingredient_string(ingredients) + ' '
+
+        answer += f"Dieses Rezept dauert circa {prep_time} Minuten und ergibt {portions} Portionen. "
+        answer += f"Die Nährwerte pro Portion sehen wie folgt aus: {nutritional_values['kcal']} kcal, {nutritional_values['protein']} g Eiweiß, {nutritional_values['fat']} g Fett und {nutritional_values['carbohydrates']} g Kohlenhydrate."
+
+        training_data.append({'prompt': prompt, 'answer': answer})
+
+    return training_data
+
+
+# Create a new JSON structure with prompts and answers for each recipe
+prompt_answer_pair = []
+skipped_recipes = []
+for recipe in tqdm(recipes):
+    # Only consider recipes with less than 15 ingredients
+    if len(recipe['ingredients']) > 15:
+        skipped_recipes.append(recipe['name'])
+        continue
+    recipe_data = {
+        "chefkochID": recipe['chefkochID'],
+        "training_data": generate_training_data(recipe)
     }
+    prompt_answer_pair.append(recipe_data)
 
-    # No list
-    # Generate prompts based on available ingredients
-    # prompts.update({
-    #    f"Prompt_{i+1}": f"Ich habe folgende Zutaten gefunden: {', '.join(ingredients[:len(ingredients)-i])}. Welches Rezept kann ich damit nachkochen?"
-    #    for i in range(len(ingredients))
-    # })
-    # output_data.append(prompts)
+print(f"Number of prompt-answer-pairs generated: {len(prompt_answer_pair)}")
+print(f"Number of skipped recipes: {len(skipped_recipes)}")
 
-    # List
-    # Generate prompts based on available ingredients and organize as a list
-    # TODO: maybe set a limit for the number of prompts
-    prompts_list = [
-        f"Ich habe folgende Zutaten gefunden: {', '.join(ingredients[:len(ingredients)-i])}. Welches Rezept kann ich damit nachkochen?"
-        for i in range(len(ingredients))
-    ]
-    prompts["Prompts"] = prompts_list  # Add prompts list to the output
+# Save the new JSON data to a file
+with open('./data/data_transformation/training_data.json', 'w', encoding="utf-8") as new_file:
+    json.dump(prompt_answer_pair, new_file,
+              ensure_ascii=False, indent=2)
 
-    # Generating Answers_to_prompts based on the last prompt and available ingredients
-    last_prompt = prompts_list[-1]
-    available_ingredients = ingredients[-1::-1]  # Reverse the ingredients list
-    answers_to_prompts = [
-        f"Wenn du noch die Zutat(en) {', '.join(available_ingredients[:i+1])} hättest, könntest du dieses Rezept probieren: {recipe['Name']}"
-        for i in range(len(available_ingredients))
-    ]
-    prompts["Answers_to_prompts"] = answers_to_prompts
-    output_data.append(prompts)
-
-# Convert to JSON and print the result for each recipe
-for recipe_output in output_data:
-    output_json = json.dumps(recipe_output, indent=4, ensure_ascii=False)
-    print(output_json)
-
-# Save the prompts to a new JSON file
-output_file = './data/data_transformation/prompts.json'
-
-with open(output_file, 'w', encoding='utf-8') as output:
-    json.dump(output_data, output, ensure_ascii=False, indent=4)
+# Save the skipped recipes to a file
+with open('./data/data_transformation/skipped_recipes.json', 'w', encoding="utf-8") as new_file:
+    json.dump(skipped_recipes, new_file,
+              ensure_ascii=False, indent=2)

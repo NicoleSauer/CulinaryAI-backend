d3a1c69dd8341697199c4be8f92058e94e41462d
Patryk Gadziomski <gadziomski.patryk@gmail.com>
2024-01-07 17:33:51 +0100

    fixed database application

--- Database Comparison/db_comperison.py
+++ Database Comparison/db_comperison.py
@@ -27,38 +27,40 @@ while True:
 
         user_ingridients = input("What ingredients do you have?: ").lower().split()
         myquery = {"ingredients.name": {"$in": user_ingridients}}
-        mydoc = mycol.find(myquery)
+        mydoc = mycol.find({})
 
-        if len(list(mydoc)) < 1:
-            print(f"No recipe with {user_ingridients} found.")
-            
-        else:
-            mydoc = mycol.find(myquery)
-            for index, recipe in enumerate(mydoc):
-                user_recipes.append(recipe)
-                print(f"[{index}] - {recipe['name']}")
-            
-            choice = int(input("Which recipe: "))
+        for doc in mydoc:
+            for name in doc['ingredients']:
+                for user_input in user_ingridients:
+                    if user_input in name['name']:
+                        user_recipes.append(doc)
 
-            recipe_name = user_recipes[choice]['name']
-            recipe_ingregdients = user_recipes[choice]['ingredients']
-            recipe_instructions = user_recipes[choice]['instructions']
+        for index, recipe in enumerate(user_recipes):
+            print(f"[{index}] - {recipe['name']}")
+    
+        choice = int(input("Which recipe: "))
 
-            print("|----------------------------------------------------------------------------------")
-            print(f"| Name: {recipe_name}")
-            print("|")
-            print("| Ingredients:")
-            for i in recipe_ingregdients:
-                if i['amount'] == None:
-                    print(f"|    - {i['name']}")
-                elif i['unit'] == None:
-                    print(f"|    - {i['name']} | {i['amount']}")
-                else:
-                    print((f"|    - {i['name']} | {i['amount']} {i['unit']}"))
-            print("|")
-            print("| Instructions:")
-            print(textwrap.fill(recipe_instructions, width=80, initial_indent="| ", subsequent_indent="| "))
-            print("|----------------------------------------------------------------------------------")
+        recipe_name = user_recipes[choice]['name']
+        recipe_ingregdients = user_recipes[choice]['ingredients']
+        recipe_instructions = user_recipes[choice]['instructions']
+
+        print("|----------------------------------------------------------------------------------")
+        print(f"| Name: {recipe_name}")
+        print("|")
+        print("| Ingredients:")
+        for i in recipe_ingregdients:
+            if i['amount'] == None:
+                print(f"|    - {i['name']}")
+            elif i['unit'] == None:
+                print(f"|    - {i['name']} | {i['amount']}")
+            else:
+                print((f"|    - {i['name']} | {i['amount']} {i['unit']}"))
+        print("|")
+        print("| Instructions:")
+        print(textwrap.fill(recipe_instructions, width=80, initial_indent="| ", subsequent_indent="| "))
+        print("|----------------------------------------------------------------------------------")
+
+        user_recipes.clear()
 
     elif user_input == 2:
         break
@@ -68,7 +70,7 @@ while True:
 
 
 '''
-Ein Nachteil der Datenbank wurde eingebaut um ihn zu verdeutlichen:
+Ein Nachteil der Datenbank, welcher möglich ist (war vorhanden):
 - "birne" wird nicht gefunden, obwohl sie als Zutat mit einem Rezept existiert
 -> Schuld daran ist, dass birne als Birne(n) gespeichert ist. Somit kann die Datenbankanwendung es nicht finden, wenn es nur nach "birne" sucht.
 
